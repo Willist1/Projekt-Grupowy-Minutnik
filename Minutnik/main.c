b26be8ae8052e4a7ef8b7173bc846a809e02b816
Willist1 <p.m.k.willist@gmail.com>
2019-02-28 10:35:08 +0100

    New project

---
+++ Minutnik/main.c
@@ -0,0 +1,18 @@
+/*
+ * Minutnik.c
+ *
+ * Created: 2019-02-28 10:27:11
+ * Author : MCPG
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
